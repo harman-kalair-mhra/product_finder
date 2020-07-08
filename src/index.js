1f492dca56cabcc0a9f6dd781242ee651893990c
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-07-08 15:27:44 +0100

    User input with validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-var readline = require("readline")
-var rl = readline.createInterface(process.stdin, process.stdout)
+var readline = require("readline");
+var rl = readline.createInterface(process.stdin, process.stdout);
 
-let itemNames = ["PL123456", "PL513872","PL999999", "PL654321", "PL818181"]
+let itemNames = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"];
 
 //TODO List: Validation
 
@@ -9,6 +9,13 @@ let itemNames = ["PL123456", "PL513872","PL999999", "PL654321", "PL818181"]
 // Check the itemsNames to see if it contains user input
 // Return a Boolean
 
-rl.question("Please enter you Medical Licence Number: ", function(answer){
-    console.log(answer)
-})
+function inputHandler(answer) {
+  let result = itemNames.includes(answer);
+  if (result) {
+    console.log("Your Licence number has been found " + answer);
+  } else {
+    console.log("Incorrect number");
+  }
+}
+
+rl.question("Please enter your Medical Licence Number: ", inputHandler);
