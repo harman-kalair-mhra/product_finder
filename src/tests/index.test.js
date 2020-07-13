7f447f72cb448af5710904becf7008393022035e
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-07-13 09:09:16 +0100

    Testing in progress

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,8 +1,8 @@
 const inputHandler = require("../index.js");
 
 describe("Product finder", function () {
-  test("Test to check User input", function () {
-    let userInput = inputHandler("PL123456");
+  test("Test to check User input mathes the Licence numbers", function () {
+    var userInput = inputHandler("PL123456");
     expect(userInput).toEqual("PL123456");
   });
 });
