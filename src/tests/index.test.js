be447d02ae1421f0b973ff8faeb11dbb65081783
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-07-13 16:59:06 +0100

    Return true and false tests complete

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,11 +1,11 @@
 let inputHandler = require("../index.js");
 
 describe("Product finder", function () {
-  test("Test to check User input mathes the Licence numbers- return true", function () {
+  test("Test to check User input matches the Licence numbers- return true", function () {
     expect(inputHandler("PL123456")).toBe(true);
   });
 
-  test("Test to check User input mathes the Licence numbers - return false", function () {
+  test("Test to check User input matches the Licence numbers - return false", function () {
     expect(inputHandler("PL137283792")).toBe(false);
   });
 });
