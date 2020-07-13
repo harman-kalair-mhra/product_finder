fc80cdd5ed39ca96c4a98886b2684883c2a445b8
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-07-13 17:15:06 +0100

    for loop to check array test

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,4 +1,5 @@
 let inputHandler = require("../index.js");
+const { interfaces } = require("mocha");
 
 describe("Product finder", function () {
   test("Test to check User input matches the Licence numbers- return true", function () {
@@ -8,4 +9,10 @@ describe("Product finder", function () {
   test("Test to check User input matches the Licence numbers - return false", function () {
     expect(inputHandler("PL137283792")).toBe(false);
   });
+
+  let itemNames = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"];
+
+  for (let i = 0; i < itemNames.length; i++) {
+    expect(inputHandler(itemNames[i])).toBe(true);
+  }
 });
