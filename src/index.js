d18939aebb22f5d9c6528554de96efe2c59fc59c
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-07-17 12:54:09 +0100

    Objects and classes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ const Product = require("./models/product.js");
 let rl = readline.createInterface(process.stdin, process.stdout);
 
 let itemNames = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"];
+product1 = new Product("PL123456", "name", "dose", "substance", "url");
+product2 = new Product("PL513872", "name", "dose", "substance", "url");
+product3 = new Product("PL999999", "name", "dose", "substance", "url");
+product4 = new Product("PL654321", "name", "dose", "substance", "url");
+product5 = new Product("PL818181", "name", "dose", "substance", "url");
+let products = [product1, product2, product3, product4, product5];
 
-function isProductPresent(product) {
-  return itemNames.includes(product);
+let productObjects = {};
+for (let i = 0; i < products.length; i++) {
+  let Product = products[i];
+  productObjects[Product.plNumber] = Product;
+}
+
+//let productfound = productObjects["PL123456"];
+//console.log(productfound);
+
+function getProduct(product) {
+  return productObjects[product];
 }
 
 function inputHandler(answer) {
-  let result = isProductPresent(answer);
+  let result = getProduct(answer);
   if (result) {
     console.log("Your Licence number has been found " + answer);
     return true;
