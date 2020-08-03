a880be83bda258371036fe5fe27ce4bb27e8c587
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-08-03 15:24:03 +0100

    Reorganising code for new class and applying best practices

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-let readline = require("readline");
-let rl = readline.createInterface(process.stdin, process.stdout);
 let ProductFinder = require("./services/finder.js");
 let getData = require("./repositories/data-provider");
-function inputHandler(answer, finder) {
-  if (answer === "Q") {
-    rl.close();
-    return null;
-  }
+let getInput = require("./app.js");
 
-  let result = finder.getProduct(answer);
-  if (result) {
-    console.log(result.toString());
-  } else {
-    console.log("Incorrect number");
-  }
-
-  rl.question(
-    "Please enter your Medical Licence Number OR press Q to Quit: ",
-    inputHandler(finder)
-  );
-}
 function startUp() {
   let finder = new ProductFinder(getData);
-  rl.question(
-    "Please enter your Medical Licence Number OR press Q to Quit: ",
-    inputHandler(finder)
-  );
+  getInput(finder);
 }
 startUp();
