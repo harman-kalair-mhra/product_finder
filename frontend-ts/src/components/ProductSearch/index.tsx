05dba528fa61b3aaedb4e523105077fd0d3690a8
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-08-28 16:48:16 +0100

    Using inState React

--- frontend-ts/src/components/ProductSearch/index.tsx
+++ frontend-ts/src/components/ProductSearch/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import styled from "styled-components";
 
 const StyledH1 = styled.h1`
@@ -7,14 +7,42 @@ const StyledH1 = styled.h1`
 `;
 
 export default function ProductSearch() {
+  let [searchedProduct, setSearchedProduct] = useState("");
+  let [searchedProducts, setSearchedProducts] = useState<string[]>([]);
+
+  const updateProduct = (e: FormEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    setSearchedProduct(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchedProducts.push(searchedProduct);
+    setSearchedProducts(searchedProducts);
+    setSearchedProduct("");
+  };
   return (
-    <form>
-      {" "}
-      <label>
-        SEARCH:
-        <input type="text" name="products" id="products" />
-      </label>
-      <input type="submit" value="SEARCH" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        {" "}
+        <label>
+          SEARCH:
+          <input
+            type="text"
+            name="products"
+            id="products"
+            value={searchedProduct}
+            onChange={updateProduct}
+          />
+        </label>
+        <input type="submit" value="SEARCH" />
+      </form>
+      <div>
+        <h2>Searched Products</h2>
+        {searchedProducts.map((product) => (
+          <p>{product}</p>
+        ))}
+      </div>
+    </div>
   );
 }
